e6e63abd78566e43470e88ac876f7826d7884704
Lucas Silva <lucasdevzx@gmail.com>
2025-09-27 11:32:39 -0300

    findByDepartment implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,6 +6,7 @@ import model.entities.Department;
 import model.entities.Seller;
 
 import java.util.Date;
+import java.util.List;
 
 public class Program {
 
@@ -16,5 +17,11 @@ public class Program {
         System.out.println("=== TEST 1: seller findById ===");
         Seller seller = sellerDao.findById(3);
         System.out.println(seller);
+
+        System.out.println("\n=== TEST 2: seller findByDepartment ===");
+        List<Seller> list = sellerDao.findBydepartment(new Department(2, null));
+        for (Seller obj : list) {
+            System.out.println(obj);
+        }
     }
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -10,7 +10,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class SellerDaoJDBC implements SellerDao {
 
@@ -35,12 +38,18 @@ public class SellerDaoJDBC implements SellerDao {
 
     }
 
+    /**
+     * Retorna tabela por id
+     *
+     * @param id fornecido pelo main e utilizado na consulta SQL
+     * @return obj retorna o resultado da consulta
+     * @throws  SQLException caso ocorra erro ao acessar o banco de dados
+     */
     @Override
     public Seller findById(Integer id) {
 
         PreparedStatement st = null;
         ResultSet rs = null;
-
         try {
             // Comando SQL de Consulta
             st = conn.prepareStatement(
@@ -48,15 +57,12 @@ public class SellerDaoJDBC implements SellerDao {
                             + "FROM seller INNER JOIN department "
                             + "ON seller.DepartmentId = department.Id "
                             + "WHERE seller.Id = ?");
-
             st.setInt(1, id); // Define um id recebido por referência do méthod para buscar a tabela no banco de dados
             rs = st.executeQuery(); // Executa o comando e retorna o resultado em formato de tabela
 
             if (rs.next()) { // Testa se a tabela tem alguma coluna diferente de zero
-
                 Department dep = instantiateDepartment(rs); // Envia o resultset como parâmetro e recebe o Department atualizado pelo method
                 Seller obj = instantiateSeller(rs, dep); // Envia o result set e o department(composição) como parâmetro e recebe o obj atualizado pelo method
-
                 return obj; // Retorna o resultado da consulta
             }
             return null; // Retorna nulo se não houver coluna diferente de zero
@@ -69,7 +75,13 @@ public class SellerDaoJDBC implements SellerDao {
         }
     }
 
-    // Reutiliza a instanciação para a atualização dos  Atributos de Department pelo Banco de Dados
+    /**
+     * Reutiliza a instanciação para a atualização dos  Atributos de Department pelo Banco de Dados
+     *
+     * @param rs parâmetro do method findbyId
+     * @return dep atualizado com os dados do banco de bados
+     * @throws SQLException dispersa exceção caso ocorra algum erro ao acessar o banco de dados
+     */
     private Department instantiateDepartment(ResultSet rs) throws SQLException{
         Department dep = new Department(); // Cria instância do Department
         // Atualizam os objetos do Department com os dados do Banco de Dados filtrado atráves do get com o nome da Tabela requerida
@@ -78,7 +90,14 @@ public class SellerDaoJDBC implements SellerDao {
         return dep;
     }
 
-    // Reutiliza a instanciação para a atualização dos Atributos do Seller pelo Banco de Dados
+    /**
+     * Reutiliza a instanciação para a atualização dos Atributos do Seller pelo Banco de Dados
+     *
+     * @param rs parâmetro do method findById
+     * @param dep parâmetro do method findById
+     * @return obj atualizado com os dados do banco de dados e objetos da classe Department
+     * @throws SQLException dispersa exceção caso ocorra algum erro ao acessar o banco de dados
+     */
     private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException{
         Seller obj = new Seller(); // Cria instância do Seller
         // Atualizam os objetos do Seller com os dados do Banco de Dados filtrado atráves do get com o nome da Tabela requerida
@@ -94,5 +113,49 @@ public class SellerDaoJDBC implements SellerDao {
     @Override
     public List<Seller> findAll() {
         return List.of();
+    }
+
+    /**
+     *
+     * @param department para conseguir o id da Classe
+     * @return list com todos os dados requeridos
+     */
+    @Override
+    public List<Seller> findBydepartment(Department department) {
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        try {
+            // Comando SQL de Consulta
+            st = conn.prepareStatement(
+                    "SELECT seller.*,department.Name as DepName "
+                    + "FROM seller INNER JOIN department "
+                    + "ON seller.DepartmentId = department.Id "
+                    + "WHERE DepartmentId = ? "
+                            + "ORDER BY Name");
+            st.setInt(1, department.getId()); // Define um id recebido por referência do méthod para buscar a tabela no banco de dados
+            rs = st.executeQuery(); // Executa o comando e retorna o resultado em formato de tabela
+
+            List<Seller> list = new ArrayList<>();
+            Map<Integer, Department> map = new HashMap<>(); // Armazenará instâncias
+
+            while (rs.next()) { // Percorre a tabela enquanto existir uma próxima coluna
+                Department dep = map.get(rs.getInt("DepartmentId")); // Verifica se um DepartmentId existe dentro do Map, se não, retorna nulo
+
+                if (dep == null) {
+                    dep = instantiateDepartment(rs); // Envia o resultset como parâmetro e recebe o Department atualizado pelo method
+                    map.put(rs.getInt("DepartmentId"), dep); // Armazena a chave do Deparment coluna atual e o Department relacionado
+                }
+
+                Seller obj = instantiateSeller(rs, dep); // Envia o result set e o department(composição) como parâmetro e recebe o obj atualizado pelo method
+                list.add(obj); // Armazena todas as colunas em uma lista
+            }
+            return list; // Retorna a lista com os dados
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+        finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
     }
 }
